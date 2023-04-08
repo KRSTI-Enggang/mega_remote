711554ff4603305575c19140ef38b163c044376d
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-09 00:54:36 +0800

    Update mega_remote.ino

--- mega_remote.ino
+++ mega_remote.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+int servoCount = 25;
+int servoPins[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
+Servo servos[25];
 
+void setup() {
+  Serial.begin(115200);
+  AttachServos();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
+
+void serialEvent() {
+  int channel;
+  int pos;
+  
+  channel = Serial.readStringUntil(':').toInt();
+  //pos = Serial.readStringUntil('*').toInt();
+  pos = Serial.readString().toInt();
+  servos[channel].write(pos);
+  Serial.print(channel  );
+  Serial.println(pos);
+}
+
+void AttachServos() {
+  for(int i = 0; i < servoCount; i++) 
+  {
+    servos[i].attach(servoPins[i]);
+  }
+ }
