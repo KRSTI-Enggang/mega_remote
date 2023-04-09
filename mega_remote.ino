e3f8e02de0c4fa17256baa15036d1dc209669658
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-09 08:13:11 +0800

    Update mega_remote.ino

--- mega_remote.ino
+++ mega_remote.ino
@@ -1,12 +1,12 @@
 #include <Servo.h>
-int servoCount = 25;
-int servoPins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
-Servo servos[25];
+Servo servo[25];
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(50);
-  AttachServos();
+  for (int i = 2; i <= 26; i++) {
+    servo[i - 2].attach(i);
+  }
 }
 
 void loop() {
@@ -16,21 +16,14 @@ void loop() {
 }
 
 void serialEvent() {
-  int channel;
+  int pin;
   int pos;
-
-  channel = Serial.readStringUntil(':').toInt();
+  pin = Serial.readStringUntil(':').toInt();
   //pos = Serial.readStringUntil('*').toInt();
   pos = Serial.readString().toInt();
-  servos[channel].write(pos);
+  servo[pin - 2].write(pos);
   Serial.print("PIN Servo : ");
-  Serial.println(channel);
+  Serial.println(pin);
   Serial.print("Derajat Servo : ");
   Serial.println(pos);
-}
-
-void AttachServos() {
-  for (int i = 0; i < servoCount; i++) {
-    servos[i].attach(servoPins[i]);
-  }
 }
